33cd68a363c933dd6dcb83b0849139b5162c03e6
Giantruler <tomboy.kooi@hotmail.com>
2015-05-12 13:34:01 +0200

    New DDL script 2.0.2

--- Database/ABCBiker DDL.sql
+++ Database/ABCBiker DDL.sql
@@ -1,3 +1,9 @@
+/*==============================================================*/
+/* DBMS name:      MySQL 5.0                                    */
+/* Created on:     5/12/2015 1:33:23 PM                         */
+/*==============================================================*/
+
+
 drop table if exists address;
 
 drop table if exists addressforcustomer;
@@ -27,9 +33,9 @@ drop table if exists workingdistrict;
 /*==============================================================*/
 create table district
 (
-  dirstrictnumber      int not null,
-  districtname         varchar(40) not null,
-  primary key (dirstrictnumber)
+   districtnumber       int not null,
+   districtname         varchar(40) not null,
+   primary key (districtnumber)
 );
 
 /*==============================================================*/
@@ -37,16 +43,16 @@ create table district
 /*==============================================================*/
 create table address
 (
-  addressnumber        int not null,
-  dirstrictnumber      int not null,
-  street               varchar(40) not null,
-  zipcode              varchar(6) not null,
-  housenumber          int not null,
-  city                 varchar(40) not null,
-  housenumberaddon     char(1),
-  primary key (addressnumber),
-  constraint fk_lies_in foreign key (dirstrictnumber)
-  references district (dirstrictnumber)
+   addressnumber        int not null,
+   dirstrictnumber      int not null,
+   street               varchar(40) not null,
+   zipcode              varchar(6) not null,
+   housenumber          int not null,
+   city                 varchar(40) not null,
+   housenumberaddon     char(1),
+   primary key (addressnumber),
+   constraint fk_lies_in foreign key (dirstrictnumber)
+      references district (districtnumber)
 );
 
 /*==============================================================*/
@@ -54,16 +60,16 @@ create table address
 /*==============================================================*/
 create table customer
 (
-  customernumber       int not null,
-  customerlastname     varchar(40) not null,
-  customerfirstname    varchar(40) not null,
-  phonenumber          numeric(14,0) not null,
-  sex                  char(1) not null,
-  companyname          varchar(40),
-  contactlastname      varchar(40),
-  contactfirstname     varchar(40),
-  email                varchar(50),
-  primary key (customernumber)
+   customernumber       int not null,
+   customerlastname     varchar(40) not null,
+   customerfirstname    varchar(40) not null,
+   phonenumber          numeric(14,0) not null,
+   sex                  char(1) not null,
+   companyname          varchar(40),
+   contactlastname      varchar(40),
+   contactfirstname     varchar(40),
+   email                varchar(50),
+   primary key (customernumber)
 );
 
 /*==============================================================*/
@@ -71,13 +77,13 @@ create table customer
 /*==============================================================*/
 create table addressforcustomer
 (
-  customernumber       int not null,
-  addressnumber        int not null,
-  primary key (customernumber, addressnumber),
-  constraint fk_reference_15 foreign key (customernumber)
-  references customer (customernumber) on delete restrict on update restrict,
-  constraint fk_reference_16 foreign key (addressnumber)
-  references address (addressnumber) on delete restrict on update restrict
+   customernumber       int not null,
+   addressnumber        int not null,
+   primary key (customernumber, addressnumber),
+   constraint fk_reference_15 foreign key (customernumber)
+      references customer (customernumber) on delete restrict on update restrict,
+   constraint fk_reference_16 foreign key (addressnumber)
+      references address (addressnumber) on delete restrict on update restrict
 );
 
 /*==============================================================*/
@@ -85,17 +91,17 @@ create table addressforcustomer
 /*==============================================================*/
 create table employee
 (
-  employeenumber       int not null,
-  addressnumber        int not null,
-  employeelastname     varchar(40) not null,
-  employeefirstname    varchar(40) not null,
-  bsn                  int not null,
-  cellphone            numeric(14,0) not null,
-  birthday             date not null,
-  sex                  char(1) not null,
-  primary key (employeenumber),
-  constraint fk_lives_at foreign key (addressnumber)
-  references address (addressnumber)
+   employeenumber       int not null,
+   addressnumber        int not null,
+   employeelastname     varchar(40) not null,
+   employeefirstname    varchar(40) not null,
+   bsn                  int not null,
+   cellphone            numeric(14,0) not null,
+   birthday             date not null,
+   sex                  char(1) not null,
+   primary key (employeenumber),
+   constraint fk_lives_at foreign key (addressnumber)
+      references address (addressnumber)
 );
 
 /*==============================================================*/
@@ -103,12 +109,12 @@ create table employee
 /*==============================================================*/
 create table biker
 (
-  employeenumber       int not null,
-  express              bool not null,
-  maxdeleveries        int not null,
-  primary key (employeenumber),
-  constraint fk_is_a2 foreign key (employeenumber)
-  references employee (employeenumber)
+   employeenumber       int not null,
+   express              bool not null,
+   maxdeleveries        int not null,
+   primary key (employeenumber),
+   constraint fk_is_a2 foreign key (employeenumber)
+      references employee (employeenumber)
 );
 
 /*==============================================================*/
@@ -116,10 +122,10 @@ create table biker
 /*==============================================================*/
 create table bus
 (
-  employeenumber       int not null,
-  primary key (employeenumber),
-  constraint fk_is_a foreign key (employeenumber)
-  references employee (employeenumber)
+   employeenumber       int not null,
+   primary key (employeenumber),
+   constraint fk_is_a foreign key (employeenumber)
+      references employee (employeenumber)
 );
 
 /*==============================================================*/
@@ -127,18 +133,18 @@ create table bus
 /*==============================================================*/
 create table consigment
 (
-  consignmentnumber    int not null,
-  customernumber       int not null,
-  deliveraddressnumber int not null,
-  pickupaddressnumber  int not null,
-  consignorname        varchar(40),
-  primary key (consignmentnumber),
-  constraint fk_placed_by foreign key (customernumber)
-  references customer (customernumber),
-  constraint fk_deliveraddress foreign key (deliveraddressnumber)
-  references address (addressnumber),
-  constraint fk_pickupaddress foreign key (pickupaddressnumber)
-  references address (addressnumber)
+   consignmentnumber    int not null,
+   customernumber       int not null,
+   deliveraddressnumber int not null,
+   pickupaddressnumber  int not null,
+   consignorname        varchar(40),
+   primary key (consignmentnumber),
+   constraint fk_placed_by foreign key (customernumber)
+      references customer (customernumber),
+   constraint fk_deliveraddress foreign key (deliveraddressnumber)
+      references address (addressnumber),
+   constraint fk_pickupaddress foreign key (pickupaddressnumber)
+      references address (addressnumber)
 );
 
 /*==============================================================*/
@@ -146,10 +152,10 @@ create table consigment
 /*==============================================================*/
 create table dispatcher
 (
-  employeenumber       int not null,
-  primary key (employeenumber),
-  constraint fk_is_a3 foreign key (employeenumber)
-  references employee (employeenumber)
+   employeenumber       int not null,
+   primary key (employeenumber),
+   constraint fk_is_a3 foreign key (employeenumber)
+      references employee (employeenumber)
 );
 
 /*==============================================================*/
@@ -157,10 +163,10 @@ create table dispatcher
 /*==============================================================*/
 create table manager
 (
-  employeenumber       int not null,
-  primary key (employeenumber),
-  constraint fk_is_a4 foreign key (employeenumber)
-  references employee (employeenumber)
+   employeenumber       int not null,
+   primary key (employeenumber),
+   constraint fk_is_a4 foreign key (employeenumber)
+      references employee (employeenumber)
 );
 
 /*==============================================================*/
@@ -168,21 +174,21 @@ create table manager
 /*==============================================================*/
 create table parcel
 (
-  consignmentnumber    int not null,
-  parcelnumber         int not null,
-  pickupemployeenumber int,
-  deliveremployeenumber int,
-  tracking             varchar(20) not null,
-  weightingrams        int not null,
-  pickuptime           datetime,
-  deliverytime         datetime,
-  primary key (consignmentnumber, parcelnumber),
-  constraint fk_is_picked_up_by foreign key (pickupemployeenumber)
-  references employee (employeenumber),
-  constraint fk_is_delivered_by foreign key (deliveremployeenumber)
-  references employee (employeenumber),
-  constraint fk_has foreign key (consignmentnumber)
-  references consigment (consignmentnumber)
+   consignmentnumber    int not null,
+   parcelnumber         int not null,
+   pickupemployeenumber int,
+   deliveremployeenumber int,
+   tracking             varchar(20) not null,
+   weightingrams        int not null,
+   pickuptime           datetime,
+   deliverytime         datetime,
+   primary key (consignmentnumber, parcelnumber),
+   constraint fk_is_picked_up_by foreign key (pickupemployeenumber)
+      references employee (employeenumber),
+   constraint fk_is_delivered_by foreign key (deliveremployeenumber)
+      references employee (employeenumber),
+   constraint fk_has foreign key (consignmentnumber)
+      references consigment (consignmentnumber)
 );
 
 /*==============================================================*/
@@ -190,12 +196,13 @@ create table parcel
 /*==============================================================*/
 create table workingdistrict
 (
-  dirstrictnumber      int not null,
-  employeenumber       int not null,
-  weeknumber           int not null,
-  primary key (dirstrictnumber, employeenumber, weeknumber),
-  constraint fk_covers_a foreign key (dirstrictnumber)
-  references district (dirstrictnumber),
-  constraint fk_number_of foreign key (employeenumber)
-  references employee (employeenumber)
+   districtnumber       int not null,
+   employeenumber       int not null,
+   weeknumber           int not null,
+   primary key (districtnumber, employeenumber, weeknumber),
+   constraint fk_covers_a foreign key (districtnumber)
+      references district (districtnumber),
+   constraint fk_number_of foreign key (employeenumber)
+      references employee (employeenumber)
 );
+
