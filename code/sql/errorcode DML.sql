51d6519466227e1eb268a7e8a62a45e88d5b8a86
Giantruler <tomboy.kooi@hotmail.com>
2015-05-13 10:15:48 +0200

    added errorcodes

--- code/sql/errorcode DML.sql
+++ code/sql/errorcode DML.sql
@@ -1,2 +1,4 @@
 INSERT INTO errorcode
-VALUES (45001, 'De naam van de klant bevat tekens!')
+VALUES (45001, 'De naam van de klant bevat tekens!'),
+       (45002, 'Geen geldig emailadres!'),
+       (45003, 'Geen geldig geslacht!')
