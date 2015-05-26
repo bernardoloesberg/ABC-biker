fe62c47e1c88a379801584a55309684ece5f2de5
Bernardo <bcm.loesberg@student.han.nl>
2015-05-26 14:52:09 +0200

    Sorting DESC on consignmentnumber

--- code/sql/consignment/vw_getConsignmentList.sql
+++ code/sql/consignment/vw_getConsignmentList.sql
@@ -9,4 +9,5 @@ CREATE VIEW vw_getConsignmentList
 AS
 SELECT co.consignmentnumber, cu.customernumber, cu.customerfirstname, cu.customerlastname, pickup.street AS pickupstreet, pickup.housenumber AS pickuphousenumber, pickup.zipcode AS pickupzipcode, pickup.city AS pickupcity, co.consignorname, co.completed, co.scheduledpickup, co.scheduleddelivery, co.price, co.totalprice FROM consignment co
 INNER JOIN customer cu ON co.customernumber = cu.customernumber
-INNER JOIN address pickup ON co.addressnumber = pickup.addressnumber;
+INNER JOIN address pickup ON co.addressnumber = pickup.addressnumber
+ORDER BY co.consignmentnumber DESC;
