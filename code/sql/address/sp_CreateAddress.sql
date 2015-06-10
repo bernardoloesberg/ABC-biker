591b809c227c7e53820b2ddb55eee18323128f5a
Giantruler <tomboy.kooi@hotmail.com>
2015-06-10 13:20:18 +0200

    added constraint to createAddress

--- code/sql/address/sp_CreateAddress.sql
+++ code/sql/address/sp_CreateAddress.sql
@@ -26,10 +26,26 @@ CREATE PROCEDURE sp_CreateAddress
           SIGNAL SQLSTATE '45009'
             SET MESSAGE_TEXT = 'Adres staat al in de database';
           ROLLBACK;
+      ELSEIF (p_street NOT REGEXP '[^a-zA-Z ]')
+        THEN
+        SIGNAL SQLSTATE '45008'
+          SET MESSAGE_TEXT = 'De straat mag alleen letters bevatten!';
+        ROLLBACK ;
       ELSEIF (p_zipcode NOT REGEXP '^[1-9][0-9]{3}\s?[a-zA-Z]{2}$')
         THEN
         SIGNAL SQLSTATE '45010'
           SET MESSAGE_TEXT = 'Geen geldig postcode!';
+        ROLLBACK ;
+      ELSEIF (p_housenumber NOT REGEXP '[0-9]')
+        THEN
+        SIGNAL SQLSTATE '45011'
+          SET MESSAGE_TEXT = 'Huisnummer mag alleen uit cijfers bestaan! Een toevoeging voor het huisnummer mag in het toevoegingsveld!';
+        ROLLBACK ;
+        ELSEIF (p_city NOT REGEXP '[^a-zA-Z ]')
+          THEN
+        SIGNAL SQLSTATE '45012'
+            SET MESSAGE_TEXT = 'De woonplaats mag alleen letters bevatten!';
+        ROLLBACK ;
       ELSE
           INSERT INTO address
           (districtnumber, street, zipcode, housenumber, city, housenumberaddon)
