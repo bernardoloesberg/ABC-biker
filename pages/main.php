46d02adb330302a1f78fa7ee3449a3c5d3351168
Bernardo <bcm.loesberg@student.han.nl>
2015-06-10 13:20:06 +0200

    fixed menu

--- pages/main.php
+++ pages/main.php
@@ -78,7 +78,7 @@ function getMenu(){
                     <!-- Collect the nav links, forms, and other content for toggling -->
                     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                       <ul class="nav navbar-nav">
-
+                        '.$dispatcher.'
                         '.(isset($_SESSION['user']) && !empty($_SESSION['user']['rolename']) && $_SESSION['user']['rolename'] == 'biker' ? $biker : '').'
                         '.(isset($_SESSION['user']) && !empty($_SESSION['user']['rolename']) && $_SESSION['user']['rolename'] == 'manager' ? $manager : '').'
                         '.(isset($_SESSION['user']) && !empty($_SESSION['user']['rolename']) && $_SESSION['user']['rolename'] == 'dispatcher' ? $dispatcher : '').'
